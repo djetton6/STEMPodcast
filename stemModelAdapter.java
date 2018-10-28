334bd9f53e3c1a56f15282a24d9c3ea095207eae
Desmend Jetton <djetton@iit.edu>
2018-10-28 00:14:46 -0500

    Creating List Adapter for STEM Models.

--- stemModelAdapter.java
+++ stemModelAdapter.java
@@ -1,7 +1,14 @@
 package com.example.android.stempodcast;
 
+import android.content.Context;
 import android.support.v7.app.AlertController;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -11,5 +18,52 @@ public class stemModelAdapter extends AlertController.RecycleListView.Adapter<st
     private LayoutInflater inflater;
     private CustomClickListener clickListener;
 
+    //create new constructor
+    public stemModelAdapter(Context context, ArrayList<stemModel> stemModels, CustomClickListener clickListener ) {
+        //construct adapter with items declared privately and new created interface
+        this.stemModels = stemModels;
+        this.clickListener = clickListener;
+        this.inflater  = LayoutInflater.from(context);
 
+
+    }
+
+    @Override
+    public @stemModelHolder onCreateViewHolder (ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.recycler_item, parent, false);
+        final_stemModelHolder stemModelHolder =  new stemModelHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //use customClickListener
+                clickListener.onItemClick(v,stemModelHolder.getLayoutPosition());
+            }
+        });
+        return stemModelHolder;
+
+
+    }
+
+    @Override
+    public void onBindViewHolder (stemModelHolder holder, int position) {
+        stemModel currentstemModel = stemModels.get(position);
+        holder.stemModel.setText(currentstemModel.getModelName());
+        holder.stemModel.setText(currentstemModel.getModelOccupation());
+        holder.stemModel.setText(currentstemModel.getModelCity());
+        //holder.stemModel.setText(currentstemModel.getModelImage());
+
+    }
+
+    @Override
+    public int getItemCount() { return stemModels.size(); }
+
+    class stemModelHolder extends RecyclerView.ViewHolder: {
+        private TextView modelName;
+        private TextView modelOccupation;
+        private TextView modelCity;
+
+        public stemModelHolder (View stemModelView) {
+
+        }
+    }
 }
